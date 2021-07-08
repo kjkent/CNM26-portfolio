a181c5fd6c3cf9aced1f611d44dd4c69a1164fea
kjkent <kris@kriskent.com>
2021-07-08 23:07:53 +0100

    Fixed hamburger

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,21 @@
 //DARK MODE
-function darkmode() {
+const darkmode = () => {
     const wasDarkmode = localStorage.getItem('darkmode') === 'true';
     localStorage.setItem('darkmode', !wasDarkmode);
     const element = document.body;
     element.classList.toggle('dark-mode', !wasDarkmode);
 }
 
-function onload() {
+const onload = () => {
     document.body.classList.toggle('dark-mode', localStorage.getItem('darkmode') === 'true');
 }
-
 window.onload = function() {onload()};
+
+const hamburgerClick = () => {
+    let x = document.getElementById("hamburgerMenu");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
